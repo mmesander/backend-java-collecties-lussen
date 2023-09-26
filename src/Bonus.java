78f076ad8322fe6e7caf8a60468879abf8b75c45
Mark Mesander <mark_mesander@hotmail.com>
2023-09-26 14:12:11 +0200

    add bonus

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,48 +1,48 @@
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Bonus {
 
     public static void main(String[] args) {
+        HashSet<Integer> secretnumber = randomnumbergenerator();
+        boolean play = true;
 
-        // Onderstaande code is uitgecommentarieerd. Haal de code uit de comment (delete /* en */) om aan de slag te gaan met de bonus
-        // In de code staan op verschillende plekken comments tussen "/*...*/" als hint wat je moet doen
+        while (play) {
+                String stringnumber = setToStringConverter(secretnumber);
+                System.out.println(stringnumber);
+                feedback(stringnumber);
+        }
 
 
-//        HashSet<Integer> secretnumber = randomnumbergenerator();
-//        String stringnumber = setToStringConverter(secretnumber);
-//        System.out.println(stringnumber);
-//        feedback(/*vul hier het juiste argument in*/);
+
+
+
+        String stringnumber = setToStringConverter(secretnumber);
+        System.out.println(stringnumber);
+        feedback(stringnumber);
 
     }
 
-    public static void/*moet dit returntype "void" zijn of wat anders?*/ randomnumbergenerator(/*Heeft deze methode nog parameter(s) nodig?*/){
-         /*
-        Vul hier de body van de methode in.
-
-        Stappenplan:
-        - Maak een nieuwe variabele van type Random. (Tip: Zoek op internet hoe je Random kunt gebruiken)
-        - Maak een nieuwe variabele van type HashSet.
-        - Schrijf een while-loop om 4 random nummers aan de hashset toe te voegen
-        - return de hashset
-         */
+    public static HashSet<Integer> randomnumbergenerator(){
+        Random randomNumber = new Random();
+        HashSet<Integer> secretNumbers = new HashSet<>();
+        while (secretNumbers.size() < 4) {
+            int secretNumber = randomNumber.nextInt(10);
+            secretNumbers.add(secretNumber);
+        }
+        return secretNumbers;
     }
 
-    public static void/*moet dit returntype "void" zijn of wat anders?*/ setToStringConverter(/*Heeft deze methode nog parameter(s) nodig?*/){
-        /*
-        Vul hier de body van de methode in.
-
-        Stappenplan:
-        - Maak als eerst een String variabele met als waarde een lege String. (of gebruik een StringBuilder)
-        - Schrijf vervolgens een for-loop om de items in de hashset een voor een aan de String variabele toe te voegen.
-        - Return de (gevulde) String variabele
-         */
+    public static String setToStringConverter(HashSet<Integer> secretnumbers) {
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < secretnumbers.size(); i++) {
+            string.append(secretnumbers);
+        }
+        return string.toString();
     }
 
 
 
-    public static void/*moet dit "void" zijn of wat anders?*/ feedback(String stringnumber) {
+    public static void feedback(String stringnumber) {
         Scanner scanner = new Scanner(System.in);
         StringBuilder feedback = new StringBuilder();
         System.out.println("+ = juiste nummer op de juiste plek, O = juiste nummer verkeerde plek, X = verkeerde nummer");
